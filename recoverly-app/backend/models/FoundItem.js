7b793fefc12f5369ab22354d0a571d9cb9fcbb53
alexjoabraham <alexjoabraham2@gmail.com>
2024-12-07 22:40:43 -0500

    Items whose claim request is approved by admin does not show in user claim items page

--- recoverly-app/backend/models/FoundItem.js
+++ recoverly-app/backend/models/FoundItem.js
@@ -8,6 +8,7 @@ const foundItemSchema = new mongoose.Schema({
   founditem_description: { type: String, required: true },
   founditem_image: { type: String }, 
   admin_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' },
+  admin_approved: { type: Boolean, default: false },
 }, {
   timestamps: true
 });
